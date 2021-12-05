94dacb9120212108b6900fc928502bca7190914f
Reynard <wkrho001@mymail.sim.edu.sg>
2021-12-05 16:41:18 +0800

    various plotting to detect outliers

--- ECB.R
+++ ECB.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(tidyr)
 library(zoo)
 library(text2vec)
+library(tidyverse)
 
 
 
@@ -33,16 +34,31 @@ fxwithspeeches$value <- na.locf(fxwithspeeches$value, na.rm=TRUE, fromLast=TRUE)
 
 summary(fxwithspeeches)
 
-#check for outliers
-plot(fxwithspeeches$date, fxwithspeeches$value, type='o', xlab='Date', 
+#check for outliers in 2 rows 2 columns
+par(mfrow=c(2,1))
+plot(fxwithspeeches$date, fxwithspeeches$value, type='l', xlab='Date', 
      ylab='Exchange Rate', main='Changes in exchange rate over time')
+hist(fxwithspeeches$value, xlab="value", main="Histogram of value", breaks = sqrt(nrow(fxwithspeeches)))
 
-boxplot(value ~ Year, data=fxwithspeeches)
+#since histogram shows possible few values that are outliers on the right side, boxplot of entire dataset is graphed
+(ggplot(fxwithspeeches) + aes(x="", y=value)+
+  geom_boxplot() )
+#no outliers in full dataset boxplot, investigating further by splicing over 10 year breaks
+fxwithspeeches %>% mutate(dategroup = cut(date, breaks = "10 years")) %>%
+  ggplot() +
+  geom_boxplot(aes(x=dategroup, y=value, color =dategroup))
+#"outliers" are present every year, however this is normal since fx is volatile daily
+fxwithspeeches %>% mutate(dategroup = cut(date, breaks = "1 year")) %>%
+  ggplot() +
+  geom_boxplot(aes(x=dategroup, y=value, color =dategroup))
 
 
-  
-  
-  
-tail(fxwithspeeches)
+#KIV- outlier detection based on z-score/IQR since "outliers do not exist in this scenario"
+#shift fxwithspeeches to x before removing outliers
+ #x <- fxwithspeeches
+ #outliers using z-score (insignificant when z>)
+ #x$z_scores <- as.data.frame(abs((x$value-mean(x$value))/sd(x$value)))
 
-head(fx,n=1)
+
+ 
+                           
